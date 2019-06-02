cf7435735e6fb36fca351482c0d93a26dbdceb3d
xkx <xiakunxian130@gmail.com>
2019-06-03 00:32:29 +0800

    include cuda-fixnum as a subdir instead of submodule

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 nvcc -g -DNDEBUG -ccbin g++ -std=c++11 -Xcompiler -Wall,-Wextra \
     --gpu-architecture=compute_35 --gpu-code=sm_35  \
-    -I./cuda-fixnum/src/ -lstdc++ -o main main.cu
+    -I./cuda-fixnum-v1/src/ -lstdc++ -o main main.cu
